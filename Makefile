475b4c45c3ae3bcc31f0ec21569dcb05211803e4
Will Smith <will@toxon.co.uk>
2018-01-28 12:48:18 +0000

    Implemented asset class filtering in get_bucket

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:
-	as date.s -o date.o
-	as bucket.s -o bucket.o
-	gcc -c test.c -o test.o -std=c99
-	gcc date.o bucket.o test.o -o test
+	as date.s -o date.o -g
+	as bucket.s -o bucket.o -g
+	gcc -c test.c -o test.o -std=c99 -g
+	gcc date.o bucket.o test.o -o test -g
 
--- bucket.s
+++ bucket.s
@@ -1,5 +1,27 @@
 .global get_bucket
 
 get_bucket:
+	// rdi = date_t* data_date
+	// rsi = asset_t* asset
+
+        mov %rdi, %r12
+        mov %rsi, %r13
+
+	// strcmp("Future", asset->asset_class) != 0 => -1
+	mov $_future, %rdi
+	mov 8(%r13), %rsi
+	call strcmp
+
+	cmp $0, %rax
+	je _is_future
+
 	mov $-1, %rax
 	ret
+
+_is_future:
+	mov $0, %rax
+	ret
+
+.data
+
+_future: .asciz "Future"
